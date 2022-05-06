f8147c205d86d69bd82d6f10a10f48167c8c243b
jeon <ouregol@naver.com>
2022-05-07 08:47:38 +0900

    rr

---
+++ React_1/js/app.js
@@ -0,0 +1,13 @@
+const sendMail = document.getElementById(`mailsend`)
+const url = require('http://localhost:8080/');
+const querystring = require('querystring');
+
+
+
+
+function sendmail(event){
+ console.log(`df`)
+}
+
+
+sendMail.addEventListener(`click`, sendmail)
