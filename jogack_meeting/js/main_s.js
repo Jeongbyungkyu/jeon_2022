5543ea68f853294995a7c8fd3ec61a87df378f16
jeon <ouregol@naver.com>
2022-05-25 17:50:46 +0900

    ㄴㅇㄹ

--- jogack_meeting/js/main_s.js
+++ jogack_meeting/js/main_s.js
@@ -10,7 +10,6 @@ function clickgo() {
   moim__y.className = `moim__y`;
   moim__m.style.animation = `test1 2s`;
   moim__m.className = `moim__m `;
-  location.reload();
 }
 
 Today.addEventListener(`click`, clickgo);
@@ -22,7 +21,6 @@ function clickmo() {
   moim__y.className = `moim__y anbogi`;
   moim__m.style.animation = `test1 2s`;
   moim__m.className = `moim__m anbogi_m`;
-  location.reload();
 }
 
 Month.addEventListener(`click`, clickmo);
